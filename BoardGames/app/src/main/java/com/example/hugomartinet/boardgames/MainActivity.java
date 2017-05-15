bf74f6fe05738aeba4e102f5bdfcdea8892af1ca
Hugo Martinet <hugo.martinet@gmail.com>
2017-05-15 13:57:23 +0200

    TitleScreen + TicTacToe

--- BoardGames/app/src/main/java/com/example/hugomartinet/boardgames/MainActivity.java
+++ BoardGames/app/src/main/java/com/example/hugomartinet/boardgames/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hugomartinet.boardgames;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +16,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView tx = (TextView)findViewById(R.id.title1);
-        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/eraser.ttf");
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/introinline.otf");
         tx.setTypeface(custom_font);
 
+        Button btn = (Button)findViewById(R.id.button1);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ActivityTicTacToe.class));
+            }
+        });
     }
 }
