27b3d2ab0e7b4c5cbf70daeb8934cb7df135ea53
Hugo Martinet <hugo.martinet@gmail.com>
2017-05-22 17:21:49 +0200

    TTT

--- BoardGames/app/src/main/java/com/example/hugomartinet/boardgames/ActivityGameTicTacToe.java
+++ BoardGames/app/src/main/java/com/example/hugomartinet/boardgames/ActivityGameTicTacToe.java
@@ -1,5 +1,6 @@
 package com.example.hugomartinet.boardgames;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,12 +10,34 @@ public class ActivityGameTicTacToe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_tic_tac_toe);
 
         TextView tx = (TextView) findViewById(R.id.titleTicTacToeGame);
         Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/introinline.otf");
         tx.setTypeface(custom_font);
+
+        TextView s1 = (TextView) findViewById(R.id.score1);
+        TextView s2 = (TextView) findViewById(R.id.score2);
+        Typeface custom_font2 = Typeface.createFromAsset(getAssets(), "fonts/Intro.otf");
+        s1.setTypeface(custom_font2);
+        s2.setTypeface(custom_font2);
+
+        System.out.println("OK");
+
+        Intent iin = getIntent();
+        Bundle b = iin.getExtras();
+
+        String name1 = (String) b.get("namePlayer1");
+        String name2 = (String) b.get("namePlayer2");
+        int numberOfPlayers = (Integer) b.get("numberOfPlayers");
+        int computerLvl = (Integer) b.get("computerLvl");
+
+        int starter = 1;
+
+        TicTacToeGame myGame = new TicTacToeGame(numberOfPlayers, starter, 1);
+
     }
 
 
